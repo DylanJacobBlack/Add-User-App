807e610793ecbe7e3ae6fd77804815975a18bf12
Dylan Black <dylan.jacob.black@gmail.com>
2021-12-14 21:17:41 +0800

    create new react projet and apply basic styles

---
+++ src/components/AddUser/NewUser.js
@@ -0,0 +1,9 @@
+import Card from '../UI/Card'
+
+const NewUser = () => {
+  return (
+    <Card>Add a new user.</Card>
+  )
+}
+
+export default NewUser;
---
+++ src/components/ListUsers/ListUsers.js
@@ -0,0 +1,7 @@
+import Card from "../UI/Card";
+
+const ListUsers = () => {
+  return <Card>List users here.</Card>;
+};
+
+export default ListUsers;
